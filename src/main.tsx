6e27eead39fc86bed4c3b25003a82972a1b07b20
Mahmoud Elkady <mahmoudallkadyy@gmail.com>
2026-01-13 17:28:36 +0200

    Enable gzip compression and switch to self-hosted fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.tsx'
 import './index.css'
+// Fonts
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 
